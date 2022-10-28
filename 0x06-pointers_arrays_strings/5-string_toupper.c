2eab7283a814b600fe8db4ae4de429f37319fd20
delakson <delakson009@gmail.com>
2022-10-28 04:08:50 -0700

    Tasks

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,16 +7,16 @@
  * Return: the pointer to dest.
  */
 
-char *string_toupper(char *s)
+char *string_toupper(char *str)
 {
-	int count = 0;
+	int index = 0;
 
-	while (*(s + count) != '\0')
+	while (str[index])
 	{
-		if (((s + count) >= 97) && ((s + count) <= 122))
-			*(s + count) = *(s + count) - 32;
-		count++;
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+			index++;
 	}
 
-	return (s);
+	return (str);
 }
